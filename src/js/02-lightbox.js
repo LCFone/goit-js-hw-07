8400c14198b51758ed26a3cc6828054161d36ac2
LCFone <putilov.k@gmail.com>
2023-08-30 22:27:37 +0300

    щтуфтвецщ

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// знаходжу контейнер для галереї в HTML
+const mainContainerGalleryRef = document.querySelector(".gallery");
+
+// створюю функцію для HTML розмітки галереї на основі масиву galleryItems
+function createGalleryCardEl(galleries) {
+
+  // беру метод map для створення рядка HTML для кожного зображення
+  return galleries
+    .map(({ preview, original, description }) => {
+      return ` <a class="gallery__item" href="${original}">
+                  <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a> `;
+    })
+    // єднаю всі нові рядки в один HTML рядок
+    .join(""); 
+}
+
+// вставляю галерею в контейнер
+mainContainerGalleryRef.insertAdjacentHTML(
+  "beforeend",
+  createGalleryCardEl(galleryItems)
+);
+
+// роблю інеціалізіцію SimpleLightbox зі зв'язком до всіх <a> елементів в галереї
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt", 
+  // затримка перед відображенням підписів
+  captionDelay: 250, 
+});
+
 console.log(galleryItems);
